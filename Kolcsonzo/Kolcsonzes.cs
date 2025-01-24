1067652c92bee328d1759a8946e164d5ff8e6f33
ArpKevin <arpaskevin11@gmail.com>
2025-01-24 13:23:58 +0100

    Add project files.

---
+++ Kolcsonzo/Kolcsonzes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolcsonzo
+{
+    internal class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public byte HajoAzonosito { get; set; }
+        public string HajoTipus { get; set; }
+        public byte SzemelyekSzama { get; set; }
+        public byte ElvitelOraja { get; set; }
+        public byte ElvitelPerce { get; set; }
+        public byte VisszahozatalOraja { get; set; }
+        public byte VisszahozatalPerce { get; set; }
+
+        public Kolcsonzes(string sor) {
+            var x = sor.Split(";");
+            Nev = x[0];
+            HajoAzonosito = byte.Parse(x[1]);
+            HajoTipus = x[2];
+            SzemelyekSzama = byte.Parse(x[3]);
+            ElvitelOraja = byte.Parse(x[4]);
+            ElvitelPerce = byte.Parse(x[5]);
+            VisszahozatalOraja = byte.Parse(x[6]);
+            VisszahozatalPerce = byte.Parse(x[7]);
+        }
+    }
+}
---
+++ Kolcsonzo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Kolcsonzo
+{
+    
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Kolcsonzes> kolcsonzesek = new();
+            foreach (var item in File.ReadAllLines(@"..\..\..\src\kolcsonzes.txt").Skip(1))
+            {
+                kolcsonzesek.Add(new Kolcsonzes(item));
+            }
+
+            Console.WriteLine("4. feladat:");
+            Console.WriteLine($"{kolcsonzesek.Count()} kölcsönzés adatai találhatók.");
+
+            Console.WriteLine("5. feladat:");
+            Console.Write("Írd be a nevet: ");
+
+            string bekertNev = Console.ReadLine();
+
+            if (kolcsonzesek.Exists(k => k.Nev == bekertNev))
+            {
+                var emberKolcsonzesei = kolcsonzesek.Where(k => k.Nev == bekertNev);
+
+                Console.WriteLine($"{bekertNev} Ettől eddig bérelt hajót/hajókat:");
+                foreach (var item in emberKolcsonzesei)
+                {
+                    Console.WriteLine($"{item.ElvitelOraja}:{item.ElvitelPerce} - {item.VisszahozatalOraja}:{item.VisszahozatalPerce}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző!");
+            }
+
+            int kulfoldiek = kolcsonzesek.Count(k => k.Nev.Contains(","));
+
+            Console.WriteLine($"{kolcsonzesek.Count - kulfoldiek} magyar, {kulfoldiek} külföldi volt aznap.");
+
+            Console.ReadKey();
+        }
+    }
+}
